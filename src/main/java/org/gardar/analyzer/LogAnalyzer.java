9d7c33432bbceb193771036e613d43bff776897d
AlGar <2677766@mail.ru>
2025-05-24 17:21:57 +1000

    Add LogParser

--- src/main/java/org/gardar/analyzer/LogAnalyzer.java
+++ src/main/java/org/gardar/analyzer/LogAnalyzer.java
@@ -1,8 +1,13 @@
 package org.gardar.analyzer;
 
+import org.gardar.analyzer.config.Config;
+import org.gardar.analyzer.parser.LogParser;
+
 public class LogAnalyzer {
 
     public static void main(String[] args) {
+        Config cfg = Config.fromArgs(args);
 
+        LogParser parser = new LogParser();
     }
 }
